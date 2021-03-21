6607858bd1c4e181814fa8ca67de19bcdc6b3269
Shubham Som <somshubham017@gmail.com>
2021-03-21 15:38:51 +0530

    added decouple in req

--- locallibrary/settings/__init__.py
+++ locallibrary/settings/__init__.py
@@ -8,23 +8,22 @@ from .common import *
 # environ.Env.read_env()
 # env = environ.Env()
 # print(list(env))
-# from decouple import config
+from decouple import config
 
-# if env['myproject'] == 'prod':
-# from .development import *
-# if config('PROJECTMODE') == 'dev':
-#     from .development import *
-# else:
-#     from .production import *
-#     # Heroku: Update database configuration from $DATABASE_URL.
-#     import dj_database_url
+
+if config('PROJECTMODE') == 'dev':
+    from .development import *
+else:
+    from .production import *
+    # Heroku: Update database configuration from $DATABASE_URL.
+    import dj_database_url
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+
+# from .production import *
+# # Heroku: Update database configuration from $DATABASE_URL.
+# import dj_database_url
 #
-#     db_from_env = dj_database_url.config(conn_max_age=500)
-#     DATABASES['default'].update(db_from_env)
-
-from .production import *
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
